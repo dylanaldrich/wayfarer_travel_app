d4b1bd54ea3c46e3502f12b5d9dad8ef2cf83d0c
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-07 19:39:43 -0400

    adds sign up modal

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('about/', views.about, name='about'),
+    path('api/', views.api, name='api'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,31 @@ from django.http import HttpResponse, JsonResponse
 # Base views
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def api(request):
+    return JsonResponse({"status": 200})
+
+# Profile views
+
+# Post views
+
+# City views
+
+
+# Signup
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
