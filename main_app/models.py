8cf81c3d992ddaf4e328717437db14497f58d6ce
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-07 18:17:00 -0400

    adds post date and join date

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
 
 # Create your models here.
 
@@ -8,24 +7,25 @@ from datetime import datetime
 
 # Profile
 class Profile(models.Model):
-    name = models.CharField(min_length=1, max_length=25, default=user)
-    city = models.CharField(min_length=1, max_length=25)
+    name = models.CharField(max_length=25)
+    # , default=user.username
+    city = models.CharField(max_length=25)
     image = models.CharField(max_length=500)
-    join_date = datetime.now()
-    user = models.OneToOneField(User)
+    join_date = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 # Posts
 class Post(models.Model):
-    title = models.CharField(min_length=1, max_length=200)
-    body = models.TextField(min_length=1, max_length=1000)
-    date = datetime.now()
+    title = models.CharField(max_length=200)
+    body = models.TextField(max_length=1000)
+    post_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-date']
+        ordering = ['-post_date']
