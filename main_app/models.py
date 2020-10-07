f80a51b89b16e9c8563e6fb7415d7fc3d3c87d48
dylanaldrich <aldrich.dylan@gmail.com>
2020-10-07 17:49:30 -0400

    Base.html created, homepage created, profile and post models created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+
+# Cities
+
+# Profile
+class Profile(models.Model):
+    name = models.CharField(min_length=1, max_length=25, default=user)
+    city = models.CharField(min_length=1, max_length=25)
+    image = models.CharField(max_length=500)
+    join_date = datetime.now()
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.name
+
+# Posts
+class Post(models.Model):
+    title = models.CharField(min_length=1, max_length=200)
+    body = models.TextField(min_length=1, max_length=1000)
+    date = datetime.now()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+# Base views
+def home(request):
+    return render(request, 'home.html')
