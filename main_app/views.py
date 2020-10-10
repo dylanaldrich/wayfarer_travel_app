42bf25dd00ad7d727d28f6ee1bf4daa3cae2f078
Beatrix Woo <beatrixwho@gmail.com>
2020-10-10 00:19:44 -0700

    city show page show 10 items at each page

--- main_app/views.py
+++ main_app/views.py
@@ -190,7 +190,16 @@ def cities_index(request):
 def cities_show(request, city_id):
     cities = City.objects.all()
     city = City.objects.get(id=city_id)
-    posts = Post.objects.filter(city=city.id)
+    posts_list = Post.objects.filter(city=city.id)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(posts_list, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    cities = City.objects.all()
     form = Post_Form(request.POST)
     context = {'cities': cities, 'city': city, 'form': form, 'posts': posts}
     return render(request, 'cities/show.html', context)
